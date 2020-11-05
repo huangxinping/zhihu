296ce742fb60267de705c3bb2bd1ad1cc3071a4a
huangxinping <o0402@icloud.com>
2020-11-05 17:34:53 +0800

    修改命令行参数配置

--- zhihuer/cli.py
+++ zhihuer/cli.py
@@ -12,7 +12,7 @@ def main():
     parser.add_argument('-i', '--id', action='store', help='问题ID，多个用","分割')
     parser.add_argument('-d', '--destination', action='store', default=os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop'), help='文件保存位置')
     parser.add_argument('-f', '--format', action='store', default='html', help='文件输出类型(html/pdf)')
-    parser.add_argument('-t', '--threshold', action='store', default=10, type=int, choices=range(1, 10000), help='点赞数阈值，低于该阈值的会过滤')
+    parser.add_argument('-t', '--threshold', action='store', default=10, type=int, help='点赞数阈值，低于该阈值的会过滤')
 
     parser.add_argument('-v', '--version', action='store_true', help='版本信息')
     parser.add_argument('-h', '--help', action='store_true', help='帮助')
